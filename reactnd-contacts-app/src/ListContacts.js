dd1eaab3c8d8254c661f50b64ffb7bcb9d29376a
RikeshThapa <rthapa@u.rochester.edu>
2020-12-07 16:57:31 -0600

    Back at it

--- reactnd-contacts-app/src/ListContacts.js
+++ reactnd-contacts-app/src/ListContacts.js
@@ -8,7 +8,7 @@ class ListContacts extends Component {
     }
 
     state = {
-        query: ''
+        query : ''
     }
 
     updateQuery = (query) => {
@@ -17,20 +17,43 @@ class ListContacts extends Component {
         }))
     }
 
+    clearQuery = () => {
+        this.updateQuery('');
+    }
+
     render() {
+        const { query } = this.state;
+        const { contacts, onDeleteContact } = this.props;
+
+        const showingContacts = query === ''
+            ? contacts
+            : contacts.filter((c) => (
+                c.name.toLowerCase().includes(query.toLowerCase())
+                ))
+
         return (
             <div className='list-contacts'>
-                <div className='lsit-contacts-top'>
+                <div className='list-contacts-top'>
                     <input
                         className = 'search-contacts'
                         type = 'text'
                         placeholder = 'Search Contacts'
-                        value = {this.state.query}
+                        value = {query}
                         onChange = {(event) => this.updateQuery(event.target.value)} 
                         />
                 </div>
+                
+                {
+                    showingContacts.length !== contacts.length && (
+                        <div className='showing-contacts'>
+                            <span> Now showing {showingContacts.length} of {contacts.length} total </span>
+                            <button onClick = {this.clearQuery}> Show All </button>
+                        </div>
+                    )
+                }                
+
                 <ol className='contact-list'>
-                    {this.props.contacts.map( contact => (
+                    {showingContacts.map( contact => (
                             <li key={ contact.id } className='contact-list-item'>
                                 <div 
                                     className='contact-avatar'
@@ -43,7 +66,8 @@ class ListContacts extends Component {
                                     </div>
                                     <button 
                                         className='contact-remove'
-                                        onClick= {() => this.props.onDeleteContact(contact)}>
+                                        onClick= {() => onDeleteContact(contact)}
+                                        >
                                         Remove
                                     </button>
                             </li>                   
