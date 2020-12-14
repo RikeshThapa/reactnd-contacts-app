473a5cd4bb12784ad80aa4cf967a2d6ee4beacc2
RikeshThapa <rthapa@u.rochester.edu>
2020-12-13 18:16:22 -0600

    Adding some routing

---
+++ reactnd-contacts-app/src/component/userRegistration.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class userRegistration extends Component{
+    render() {
+        return(
+            <div>Create Contact</div>
+        )
+    }
+
+}
+
+
+export default userRegistration;
