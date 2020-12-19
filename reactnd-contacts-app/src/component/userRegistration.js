1c55d4a3ba9b5e890005e3e5f58c0bd2aa1b0e91
RikeshThapa <rthapa@u.rochester.edu>
2020-12-19 14:13:05 -0600

    Adding fields to userRegistration

--- reactnd-contacts-app/src/component/userRegistration.js
+++ reactnd-contacts-app/src/component/userRegistration.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ImageInput from '../ImageInput';
 
 
 class userRegistration extends Component{
@@ -10,6 +11,18 @@ class userRegistration extends Component{
                 className="close-create-contact"
                 to='/'>
                     Close</Link>
+                <form className='create-contact-form'>
+                    <ImageInput 
+                        className='creat-contact-avatar-input'
+                        name='avatarURL'
+                        maxHeight={64}
+                        />
+                    <div className='create-contact-details'>
+                        <input type='text' name='name' placeholder='Name' />
+                        <input type='text' name='handle' placeholder='Handle' />
+                        <button>Add Contact</button>
+                    </div>
+                </form>
             </div>
         )
     }
