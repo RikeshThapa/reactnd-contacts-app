50eb73edcd8bade890a2a76d43826210bcc46bae
RikeshThapa <rthapa@u.rochester.edu>
2020-12-18 21:29:01 -0600

    Add back button to userRegistration

--- reactnd-contacts-app/src/component/userRegistration.js
+++ reactnd-contacts-app/src/component/userRegistration.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class userRegistration extends Component{
     render() {
         return(
-            <div>Create Contact</div>
+            <div>
+                <Link 
+                className="close-create-contact"
+                to='/'>
+                    Close</Link>
+            </div>
         )
     }
 }
